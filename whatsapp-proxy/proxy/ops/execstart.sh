c859f74bba39220164d6c84169ae036817af9dbb
Adrian Gri <adrian.gri03@gmail.com>
2023-01-09 15:53:16 -0500

    update path in script

--- whatsapp-proxy/proxy/ops/execstart.sh
+++ whatsapp-proxy/proxy/ops/execstart.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 service docker start
-docker-compose -f /home/ec2-user/proxy/proxy/ops/docker-compose.yml up -d
+docker-compose -f /home/ec2-user/ProxyNow/whatsapp-proxy/proxy/ops/docker-compose.yml up -d
